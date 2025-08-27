9ca063ae23e8c4f683f77cce9266b2eb119157c1
hjy080530 <bomaye119@gmail.com>
2025-08-28 00:33:18 +0900

    feat:: 파일 설정 추가
    # Conflicts:
    #	README.md

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import Providers from "./providers";
+
+export const metadata: Metadata = {
+    title: "My App",
+    description: "Next.js + Emotion + React Query",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
