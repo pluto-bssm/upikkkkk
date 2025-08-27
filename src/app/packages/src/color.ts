a6fed4ec979b67c4029548ff8e7e687acc5d5fe9
김태현 <143389711+taehyun00@users.noreply.github.com>
2025-08-28 08:43:22 +0900

    feat :: packages add

---
+++ src/app/packages/src/color.ts
@@ -0,0 +1,19 @@
+const color = {
+  white: '#FDFFFC',
+  black: '#011627',
+
+
+  gray600: '#616161',
+  gray500: '#777777',
+  gray400: '#B3B3B3',
+  gray300: '#C8C8C8',
+  gray200: '#DADADA',
+  gray100: '#F8F8F9',
+  gray50: '#F1F1F1',
+
+  upikorange : '#FF9F1C',
+  upikblue : '#58CCFF',
+  upikred : '#E71D36',
+};
+
+export default color;
