4a12d00aa16409d21a961076d02dadc22721fbb9
anminju08 <anminju0310@gmail.com>
2025-09-11 00:05:49 +0900

    add PopularVote.tsx and style ts fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,10 @@ import NavigationBar from '@/components/common/NavigationBar'
 import styled from '@emotion/styled'
 import HeaderItemsBox from '@/components/Header/HeaderItemBox'
 import color from '@/packages/design-system/src/color'
+import font from '@/packages/design-system/src/font'
 import RecoVote from '@/components/Main/RecoVote'
-import MainComponent from '@/components/Main/MainComponent'
+import MainComponent from '@/components/Main/GuideComponent'
+import PopularVote from '@/components/Main/PopularVote';
 
 const Main = () => {
   return (
@@ -20,9 +22,17 @@ const Main = () => {
       } 
         RightItem={<HeaderItemsBox type={'main'} />}
         types='None'/>
-
         <RecoVote/>
+        <SectionHeader>
+          <Book />
+          <SectionTitle>인기 가이드</SectionTitle>
+        </SectionHeader>
         <MainComponent/>
+        <SectionHeader>
+          <VoteMain />
+          <SectionTitle>인기 투표</SectionTitle>
+        </SectionHeader>
+        <PopularVote />
       <NavigationBar />
     </MainPageLayout>
   );
@@ -42,9 +52,33 @@ const MainPageLayout = styled.div`
   
 `
 
+const SectionHeader = styled.div`
+  display: flex;
+  align-items: center;
+  height: 37px;
+  gap: 8px;
+  margin-top:20px;
+  align-self:flex-start;
+  margin-left:6%;
+`;
 
-const ContentArea = styled.div`
-  width: 100%;
-  flex: 1;
-  margin-top : 100px;
+const SectionTitle = styled.div`
+  color: ${color.gray700};
+  font-family:${font.P2};
+`;
+
+const Book = styled.div`
+  width:24px;
+  height:22px;
+  background-color:${color.gray700};
+  -webkit-mask: url('/svg/Book.svg') no-repeat center / contain;
+  mask: url('/svg/Book.svg') no-repeat center / contain;
+`;
+
+const VoteMain = styled.div`
+  width:24px;
+  height:22px;
+  background-color:${color.gray700};
+  -webkit-mask: url('/svg/VoteMain.svg') no-repeat center / contain;
+  mask: url('/svg/VoteMain.svg') no-repeat center / contain;
 `;
