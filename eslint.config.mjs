789bc634031263866b45a4981c24efda5bca03c0
김태현 <143389711+taehyun00@users.noreply.github.com>
2025-09-20 08:41:50 +0900

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,12 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off", 
+
+    },
+  },
 ];
 
 export default eslintConfig;
